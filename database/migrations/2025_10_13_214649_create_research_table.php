b6d9a43c9a33330d05c10b2cfce4e5e116f04485
fzengin19 <fatihzengin654@outlook.com>
2025-10-14 01:19:46 +0300

    Add Research model and migration for research table

---
+++ database/migrations/2025_10_13_214649_create_research_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('research', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->string('title');
+            $table->mediumText('additional_info');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('research');
+    }
+};
